4cc4b592a198abc77f405ad4393dd505e33cfab3
Vysakh P <vysakhp17@gmail.com>
2024-10-29 22:45:12 +0530

    Refactor app.config.ts and app.routes.ts
    - Refactor app.config.ts to improve code readability and maintainability.
    - Update app.routes.ts to add routing configuration for authentication.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent)
+            },
+            {
+                path: '',
+                redirectTo: 'login',
+                pathMatch: 'full'
+            }
+        ]
+    },
+];
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  protected loginForm: FormGroup<{
+    email: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }> = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  protected onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 
 }
